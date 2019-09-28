e6526f94416d6fce721cf3193f2a343bbc20d744
niQQe <44671699+niQQe@users.noreply.github.com>
2019-09-28 18:04:39 +0200

    Added fetch instead of jquery ajax

--- niqqeparse.js
+++ niqqeparse.js
@@ -1,36 +1,32 @@
 NiqqeParse = {
-    Parse(file, callback){
-      $.ajax({
-        url: file,
-        dataType: "text",
-        success: function(data) {
-          getHeaders(data);
+  Parse(file, callback){
+    fetch(file)
+    .then(response => response.text())
+    .then(csv => getHeaders(csv))
+    
+    function getHeaders(rawdata) {
+      let headers = rawdata.split('\n')[0].replace('\r', '').split(';');
+      let data = rawdata.split('\n');
+      let puredata = [];
+      for (let i = 1; i < data.length; i++) {
+        if (data[i].length > '') {
+          puredata.push(data[i].replace('\r', '').split(';'));
         }
-      });
-      
-      function getHeaders(rawdata) {
-        let headers = rawdata.split('\n')[0].replace('\r', '').split(';');
-        let data = rawdata.split('\n');
-        let puredata = [];
-        for (let i = 1; i < data.length; i++) {
-          if (data[i].length > '') {
-            puredata.push(data[i].replace('\r', '').split(';'));
-          }
-        }
-        pushData(puredata, headers);
       }
-      
-      function pushData(puredata, keys) {
-        var temp = {};
-        var finishedArr = [];
-        for (var i = 0; i < puredata.length; i++) {
-          for (var j = 0; j < keys.length; j++) {
-            temp[keys[j]] = puredata[i][j];
-          }
-          finishedArr.push(temp);
-          temp = {};
+      pushData(puredata, headers);
+    }
+    
+    function pushData(puredata, keys) {
+      var temp = {};
+      var finishedArr = [];
+      for (var i = 0; i < puredata.length; i++) {
+        for (var j = 0; j < keys.length; j++) {
+          temp[keys[j]] = puredata[i][j];
         }
-        callback(finishedArr);
+        finishedArr.push(temp);
+        temp = {};
       }
+      callback(finishedArr);
     }
   }
+}
