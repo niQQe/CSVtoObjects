2de7305a684b7f35d426579adaacea4a35192625
niQQe <44671699+niQQe@users.noreply.github.com>
2019-09-28 16:08:17 +0200

    initial commit

---
+++ niqqeparse.js
@@ -0,0 +1,36 @@
+NiqqeParse = {
+    Parse(file, callback){
+      $.ajax({
+        url: file,
+        dataType: "text",
+        success: function(data) {
+          getHeaders(data);
+        }
+      });
+      
+      function getHeaders(rawdata) {
+        let headers = rawdata.split('\n')[0].replace('\r', '').split(';');
+        let data = rawdata.split('\n');
+        let puredata = [];
+        for (let i = 1; i < data.length; i++) {
+          if (data[i].length > '') {
+            puredata.push(data[i].replace('\r', '').split(';'));
+          }
+        }
+        pushData(puredata, headers);
+      }
+      
+      function pushData(puredata, keys) {
+        var temp = {};
+        var finishedArr = [];
+        for (var i = 0; i < puredata.length; i++) {
+          for (var j = 0; j < keys.length; j++) {
+            temp[keys[j]] = puredata[i][j];
+          }
+          finishedArr.push(temp);
+          temp = {};
+        }
+        callback(finishedArr);
+      }
+    }
+  }
